887d7f9febd0e92f9868672b89dea55ea14150c2
Wingwaresoftware <127249068+Wingwaresoftware@users.noreply.github.com>
2023-05-13 23:27:10 -0300

    Update __init__.py

--- DailyPic/__init__.py
+++ DailyPic/__init__.py
@@ -5,7 +5,7 @@ import json
 from .config import UNSPLASH_ACCESS_KEY
 
 
-def dailypic():
+def dailyimg():
     fecha_actual = datetime.date.today().strftime('%Y-%m-%d')
     url = f'https://api.unsplash.com/photos/random?query=landscape&orientation=landscape&client_id={UNSPLASH_ACCESS_KEY}'
     respuesta = requests.get(url)
